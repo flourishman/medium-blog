a05b9b26c34e03d204ca2efe8bd57d1503f58418
Sulyman Hammed <sulymanhammed2015@gmail.com>
2024-01-30 15:13:34 +0100

    Demo header added

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import Demo from "./components/Demo/Demo";
+import HomeHeader from "./components/Home/HomeHeader";
+import DemoHeader from "./components/Demo/DemoHeader";
+
 function App() {
+  const auth = false;
   return (
     <>
+      {auth ? <HomeHeader /> : <DemoHeader />}
       <div>
-        <h1 className="text-red-600">Hello</h1>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+        </Routes>
       </div>
     </>
   );
---
+++ src/data.js
@@ -0,0 +1,14 @@
+export const nav = [
+  {
+    title: "Our Story",
+    path: "/",
+  },
+  {
+    title: "Membership",
+    path: "/",
+  },
+  {
+    title: "Write",
+    path: "/",
+  },
+];
